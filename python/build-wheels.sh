214ff0e2d70bd38f22ef2a0291fea9256fbaf591
Easton Potokar <contagon6@gmail.com>
2021-05-15 14:48:08 -0600

    Got all pypi pipeline working!

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-CURRDIR=$(pwd)
-
-# Set the build directory 
-BUILDDIR="/io/build"
-mkdir -p ${BUILDDIR}
-
-# FIX auditwheel
-# https://github.com/pypa/auditwheel/issues/136
-# echo /opt/_internal/*/*/*/*/auditwheel
-# cd /opt/_internal/tools/lib64/python3.7/site-packages/auditwheel
-# patch -p2 < python/auditwheel.txt
-# cd $BUILDDIR
+SRCDIR=$(pwd)
 
 # Get python information
 PYBIN="/opt/python/$PYTHON_VERSION/bin"
 PYVER_NUM=$($PYBIN/python -c "import sys;print(sys.version.split(\" \")[0])")
+PYTHONVER="$(basename $(dirname $PYBIN))"
 export PATH=$PYBIN:$PATH
 
+# Set the build directory 
+BUILDDIR="$SRCDIR/build-$PYVER_NUM"
+mkdir -p ${BUILDDIR}
+
 # Install python dependencies
-${PYBIN}/pip install -r python/requirements.txt
+# ${PYBIN}/pip install -r python/requirements.txt
 
 # run cmake & make
 cd ${BUILDDIR}
-cmake ${CURRDIR} -DCMAKE_BUILD_TYPE=Release \
+cmake ${SRCDIR} -DCMAKE_BUILD_TYPE=Release \
     -DPYTHON=ON \
     -DTESTS=OFF \
     -DEXAMPLES=OFF \
@@ -31,27 +25,27 @@ cmake ${CURRDIR} -DCMAKE_BUILD_TYPE=Release \
     -DAUG=1 \
     -DVEC=1 \
     -DPYBIND11_PYTHON_VERSION=$PYVER_NUM; ec=$?
-# if [ $ec -ne 0 ]; then
-#     echo "Error:"
-#     cat ./CMakeCache.txt
-#     exit $ec
-# fi
-set -e -x
 make -j2
 
-# make wheels
-mkdir -p /io/wheelhouse
+# make wheel
+# outputs them in BUILDDIR/python/dist
 cd python
 "${PYBIN}/python" setup.py bdist_wheel --python-tag=$PYTHONVER --plat-name=$PLAT
 
-# Bundle external shared libraries into the wheels
+# Bundle external shared libraries into the wheel
+# outputs them in BUILDDIR/python/wheelhouse
 for whl in ./dist/*.whl; do
-    auditwheel repair "$whl" -w /io/wheelhouse/
+    echo $(auditwheel show $whl)
+    auditwheel repair "$whl"
 done
 
-for whl in /io/wheelhouse/*.whl; do
+# rename wheel
+# move it to SRCDIR/dist
+mkdir -p $SRCDIR/dist
+for whl in ./wheelhouse/*.whl; do
     new_filename=$(echo $whl | sed "s#\.none-manylinux2014_x86_64\.#.#g")
     new_filename=$(echo $new_filename | sed "s#\.manylinux2014_x86_64\.#.#g") # For 37 and 38
     new_filename=$(echo $new_filename | sed "s#-none-#-#g")
     mv $whl $new_filename
+    mv $new_filename $SRCDIR/dist/
 done
